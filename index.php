944e1575276e62d6adee281b70f86d09846e4e1f
ness1307 <a.ness1307@gmail.com>
2019-05-14 16:08:26 +0200

    ajout d'un doossier training

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <b style='color: red;'>texte en rouge</b> 
+
+</body>
+</html>
+
+<?php
+
+$age_du_visiteur = 25;
+$prenom = 'je m\'appele le nouveau';
+$color = 'red';
+
+
+
+
+$heure = 22;
+
+if ($heure <=19)
+{
+   echo "Bonjour à toi !<br />";
+
+}
+
+else 
+{
+    echo "Bonsoir à toi !<br />";
+}
+
+
+
